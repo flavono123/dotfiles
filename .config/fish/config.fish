8bad1ae84165f68ba6a666c2c9138d8c26441afa
flavono123 <flavono123@gmail.com>
2019-04-26 13:03:52 +0900

    gitconfig
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 alias nv 'nvim'
 alias dotfile '/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
+# Fish (Space Fish)
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+
 # Ruby on Rails
 set -gx PATH ~/.rbenv/shims $PATH
 set -gx RAILS_ENV development
