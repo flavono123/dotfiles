e702e1ecf431adc9e041d38d9b8631650c4dd59d
flavono123 <flavono123@gmail.com>
2022-10-18 23:13:52 +0900

    neovim: Install LSP for Bash
    - Install nvim-lspconfig, mason, mason-lspconfig
      - https://github.com/williamboman/mason-lspconfig.nvim#installation
    - Install bash-language-server, shellcheck(from Homebrew already)
      -> Lint(?) SCXXX roles
    - Config suggestions(https://github.com/neovim/nvim-lspconfig#suggested-configuration)
    
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -12,6 +12,10 @@ Plug 'hashivim/vim-terraform'
 
 Plug 'tmux-plugins/vim-tmux-focus-events'
 
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'neovim/nvim-lspconfig'
+
 call plug#end()
 
 " 무인도에 들고 갈 옵션
@@ -69,3 +73,47 @@ set autoread
 " to Pmenu, change it as vim, to be same with Normal
 hi link NormalFloat Normal
 ]])
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+
+require('lspconfig')['bashls'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+}
