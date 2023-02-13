8829b68359716aade756a5769e2cb00a2bd336de
flavono123 <flavono123@gmail.com>
2023-02-13 17:19:00 +0900

    dap: resize UI layout
    - remove console
    - left(25% of widths) side bar
      - watches
      - stacks
      - breakpoints
      - scopes
    - botton(25% of heights): repl
    
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- neovim/.config/nvim/lua/config/dap/init.lua
+++ neovim/.config/nvim/lua/config/dap/init.lua
@@ -29,7 +29,20 @@ local function configure_exts()
   require('nvim-dap-virtual-text').setup { commented = true }
 
   local dap, dapui = require 'dap', require 'dapui'
-  dapui.setup {} -- use default
+
+  local layouts = {
+    {
+      elements = {
+        { id = 'scopes', size = 0.25 }, { id = 'breakpoints', size = 0.25 },
+        { id = 'stacks', size = 0.25 }, { id = 'watches', size = 0.25 },
+      },
+      position = 'left',
+      size = 0.25,
+    },
+    { elements = { { id = 'repl', size = 1 } }, position = 'bottom',
+      size = 0.25 },
+  }
+  dapui.setup { layouts = layouts }
   dap.listeners.after.event_initialized['dapui_config'] = function()
     dapui.open()
   end
