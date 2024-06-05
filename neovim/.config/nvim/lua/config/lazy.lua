42e817b5e4fb759c47785a8438110334734b15b4
flavono123 <flavono123@gmail.com>
2024-06-05 15:45:42 +0900

    feat(nvim): install lazyvim terraform extra
    https://www.lazyvim.org/extras/lang/terraform
    
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
