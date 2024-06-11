e59c7b41b64ec9f81fa67e56224f068ac9d365f9
flavono123 <flavono123@gmail.com>
2024-06-11 23:21:05 +0900

    fix(nvim): use bufferline cmd over tabp, tabN
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+-- override tabs
+-- i think bufferline override or spoil the default tabp and tabN ¯\_(ツ)_/¯
+map("n", "<tab>", "<cmd>BufferLineCycleNext<cr>")
+map("n", "<S-tab>", "<cmd>BufferLineCyclePrev<cr>")
