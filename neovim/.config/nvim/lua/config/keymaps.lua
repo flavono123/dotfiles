5115147449bce58ea9990ede3e3733b5075ba444
flavono123 <flavono123@gmail.com>
2024-06-20 10:34:43 +0900

    feat(nvim): add keymaps
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -3,10 +3,16 @@
 -- Add any additional keymaps here
 
 local map = LazyVim.safe_keymap_set
+
+map("n", "<c-q>", "<cmd>q<cr>")
+map("n", "<c-n>", "<cmd>enew<cr>")
 -- override tabs
 -- i think bufferline override or spoil the default tabp and tabN ¯\_(ツ)_/¯
 map("n", "<tab>", "<cmd>BufferLineCycleNext<cr>")
 map("n", "<S-tab>", "<cmd>BufferLineCyclePrev<cr>")
 
+map("v", "<c-c>", '"ay')
+map("i", "<c-v>", '<esc>"ap')
+
 map("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
-map("n", "<c-n>", "<Plug>(YankyNextEntry)")
+-- map("n", "<c-n>", "<Plug>(YankyNextEntry)")
