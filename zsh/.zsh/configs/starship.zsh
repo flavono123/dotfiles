e15d9263811c12597631efa2873754207604d63d
flavono123 <flavono123@gmail.com>
2024-11-19 08:53:42 +0900

    fix(starship): use "default" by default
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- zsh/.zsh/configs/starship.zsh
+++ zsh/.zsh/configs/starship.zsh
@@ -1,3 +1,5 @@
 # hot swap; export STARSHIP_CONFIG=~/.configs/<type>.toml
 # ref: https://starship.rs/config/#config-file-location
+
+export STARSHIP_CONFIG=$HOME/.config/starship/default.toml # even "default.toml" is not the default path
 eval "$(starship init zsh)"
